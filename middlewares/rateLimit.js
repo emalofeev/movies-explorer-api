5ff4cb73c021fb8dcfe6b1458d60e0cccca95180
Evgeniy Malofeev <euge.ma@yandex.ru>
2023-06-10 15:53:37 +0300

    feat: modification rateLimit

--- middlewares/rateLimit.js
+++ middlewares/rateLimit.js
@@ -1,8 +1,8 @@
 const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
+  windowMs: 1 * 60 * 1000,
+  max: 8000,
 });
 
 module.exports = limiter;
